2349108183d57e20797550d37acf80418a5f403f
Alexis Dutoya <alexis.dutoya@gmail.com>
2024-04-05 17:42:08 +0200

    working scrapper

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 require "nokogiri"
 require "httparty"
 require "csv"
+require "open-uri"
 
 # target page
 response = HTTParty.get("https://www.lavillarose.fr/nos-voitures/", {
@@ -10,7 +11,7 @@ response = HTTParty.get("https://www.lavillarose.fr/nos-voitures/", {
 })
 
 # store the collected data
-Voiture = Struct.new(:image, :url, :name)
+Voiture = Struct.new(:date, :image, :url, :name, :year, :price, :km)
 
 # scraping logic
 doc = Nokogiri::HTML(response.body)
@@ -27,15 +28,35 @@ voiture_cards.each do |voiture_card|
 
     # extract the data of interest
 
+    date = Time.now
+
     image = voiture_card.at_css(".oxy-carousel-builder_gallery-image img").attribute("src").value
 
     url = voiture_card.at_css(".div-liste-infos-voitures").attribute("href").value
 
     name = voiture_card.at_css(".div-titre-annee-voiture h2").text
 
+    year = voiture_card.at_css(".annee-voiture span").text
+
+    # if the price exists
+    price_element = voiture_card.at_css(".prix-voiture span")
+    if price_element
+        price = price_element.text
+    else
+        # Check if the car is sold
+        vendu_element = voiture_card.at_css(".div-vendu")
+        if vendu_element
+            price = "Vendu"
+        else
+            price = "Réservée"
+        end
+    end
+
+    km = voiture_card.at_css(".liste-infos-voiture span").text
+
     # instantiate an Voiture object with the collected data
 
-    voiture = Voiture.new(url, image, name)
+    voiture = Voiture.new(date, url, image, name, year, price, km)
 
     # add the Voiture instance to the array of scraped objects
 
@@ -49,7 +70,7 @@ CSV.open("output.csv", "wb") do |csv|
 
   # write the CSV header
 
-  csv << ["url", "image", "name"]
+  csv << ["date", "url", "image", "name", "year", "price", "km"]
 
   # transfrom each voiture scraped info to a CSV record
 
